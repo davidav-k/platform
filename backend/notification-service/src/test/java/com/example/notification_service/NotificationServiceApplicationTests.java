8463460b884061b7d65450e3dd8595ab11e96a7d
davidav <davidenkav@gmail.com>
2026-06-08 15:43:16 +0500

    Added Config Server runtime configuration and Flyway-managed PostgreSQL database

--- backend/notification-service/src/test/java/com/example/notification_service/NotificationServiceApplicationTests.java
+++ backend/notification-service/src/test/java/com/example/notification_service/NotificationServiceApplicationTests.java
@@ -7,7 +7,11 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(properties = {
         "spring.cloud.config.enabled=false",
         "eureka.client.enabled=false",
-        "server.port=0"
+        "server.port=0",
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
 @ActiveProfiles("test")
 class NotificationServiceApplicationTests {
